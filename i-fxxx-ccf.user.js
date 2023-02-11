55d3feeb675147010cbf29bc36a477b425a88dd1
zhegeshijieshangzuishuaideren <89591148+zhegeshijieshangzuishuaideren@users.noreply.github.com>
2023-02-11 12:22:30 +0800

    Update i-fxxx-ccf.user.js

--- i-fxxx-ccf.user.js
+++ i-fxxx-ccf.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         I-Fxxx-CCF
 // @namespace    https://€€£.fucksc.cf/
-// @version      0.1
+// @version      0.2
 // @description  将中国计算机学会官网所有的 CCF 改成 €€£
 // @author       €€£
 // @match        *://*.ccf.org.cn/*
@@ -10,12 +10,13 @@
 // @match        *://*.yocsef.org.cn/*
 // @icon         https://avatars.githubusercontent.com/u/91953449
 // @grant        none
-// @homepage     https://github.com/dsy4567/I-Fxxx-CCF
+// @homepage     https://github.com/fuckccf/I-Fxxx-CCF
 // @license      WTFPL
 // ==/UserScript==
 
 (function() {
-let kwd={
+
+var kwd = {
     "GESP": "₲E$₽",
     "Grade": "₲rade",
     "Programming": "₽rogramming",
@@ -33,25 +34,33 @@ let kwd={
     "Federation": "£ederation"
 }
 
-Object.keys(kwd).forEach(k=>{
-    document.querySelectorAll("div,p,a,h1,h2,h3,h4,h5,h6,span")?.forEach((el)=>{
-        if (el.innerHTML.includes(k))
+Object.keys(kwd).forEach(k => {
+    document.querySelectorAll("div,p,a,h1,h2,h3,h4,h5,h6,span")?.forEach((el) => {
+        if (el.innerHTML.includes(k) && !el.innerHTML.includes("版权"))
             el.innerHTML = el.innerHTML.replaceAll(k,kwd[k])
     })
     document.title = document.title.replaceAll(k,kwd[k])
 })
-Object.values(kwd).forEach(k=>{
-    document.querySelectorAll("*[href], *[src]")?.forEach((el)=>{
-        if (el.getAttribute("href")?.includes(k))
-            el.setAttribute("href", el.getAttribute("href".replaceAll(k,Object.keys(kwd.valueOf(k))[0])))
-        if (el.getAttribute("src")?.includes(k))
-            el.setAttribute("src", el.getAttribute("src".replaceAll(k,Object.keys(kwd.valueOf(k))[0])))
+Object.keys(kwd).forEach(k => {
+    document.querySelectorAll("*[href], *[src]")?.forEach((el) => {
+        if (el.href?.includes(encodeURI(kwd[k])))
+            el.href = decodeURI(el.href).replaceAll(kwd[k],k)
+        if (el.src?.includes(encodeURI(kwd[k])))
+            el.src = decodeURI(el.src).replaceAll(kwd[k],k)
     })
 })
 document.querySelector(".header .logo")?.remove()
-let img = document.createElement("img")
+var img = document.createElement("img")
 img.src = "https://avatars.githubusercontent.com/u/91953449";
 img.alt = "中国收钱协会"
 img.className = "logo"
 document.querySelector(".header .tt")?.append(img)
+document.body.insertAdjacentHTML("beforeend", `
+<style>
+body > div.index-xf {
+    display: none !important;
+}
+</style>
+`)
+
 })();
